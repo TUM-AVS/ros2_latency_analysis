3db276c6697d70290d481fb2c72e95de19dffc23
Maximilian Schmeller <m.schmeller@tum.de>
2022-06-28 21:27:15 +0200

    Process lambda callbacks, annotate member refs with L/GL/PR/X-value tags

--- MemberRefCollector.cpp
+++ MemberRefCollector.cpp
@@ -1,21 +1,34 @@
 #include "MemberRefCollector.hh"
 
 void MemberRefCollector::run(const MatchFinder::MatchResult &Result) {
-    if (const CallExpr *expr = Result.Nodes.getNodeAs<clang::CallExpr>("create_sub_call")) {
-        if (expr->getNumArgs() < 3) {
-            std::cout << "Expression has " << expr->getNumArgs() << " args, expected 3 or more. Ignoring." << std::endl;
+    if (const CallExpr *CreateSubCall = Result.Nodes.getNodeAs<clang::CallExpr>("create_sub_call")) {
+        if (CreateSubCall->getNumArgs() < 3) {
+            std::cout << "Expression has " << CreateSubCall->getNumArgs() << " args, expected 3 or more. Ignoring."
+                      << std::endl;
             return;
         }
-        const Expr *CallbackArg = expr->getArg(2);
-        const DeclRefExpr *CallbackSig = this->unwrapCallbackArg(CallbackArg);
-        this->getCallbackDefinition(CallbackSig);
+        const Expr *CallbackArg = CreateSubCall->getArg(2);
+        const Expr *CallbackBody = this->unwrapCallbackArg(CallbackArg);
+        if (CallbackBody == nullptr) return;
+
+        std::vector<MemberExpr *> CallbackMemberRefs;
+        this->getCallbackMemberRefs((Expr *) CallbackBody, CallbackMemberRefs);
+        for (MemberExpr *MemberRef: CallbackMemberRefs) {
+            std::cout << MemberRef->getMemberNameInfo().getAsString()
+                    << "---"
+                    << " L:" << MemberRef->isLValue()
+                    << " GL:" << MemberRef->isGLValue()
+                    << " PR:" << MemberRef->isPRValue()
+                    << " X:" << MemberRef->isXValue()
+                    << std::endl;
+        }
 
         std::cout << std::endl;
         std::cout << std::endl;
     }
 }
 
-const DeclRefExpr *MemberRefCollector::unwrapCallbackArg(const Expr *CallbackArg) {
+const Expr *MemberRefCollector::unwrapCallbackArg(const Expr *CallbackArg) {
     if (!checkClass(CallbackArg, Stmt::StmtClass::MaterializeTemporaryExprClass,
                     "Callback arg wrapper is not a temporary expression"))
         return nullptr;
@@ -24,9 +37,11 @@ const DeclRefExpr *MemberRefCollector::unwrapCallbackArg(const Expr *CallbackArg
     const Expr *TheChild = getChild(CallbackArg, 0, 1);
     if (!checkClass(TheChild, Stmt::StmtClass::CallExprClass,
                     "Callback arg is not a bind call")) {
-        TheChild->dumpColor();
+        if (!checkClass(TheChild, Stmt::StmtClass::LambdaExprClass,
+                        "  and also not a lambda")) return nullptr;
+
+        return TheChild; // Child is a LambdaExpr and is its own function body
     }
-    return nullptr;
 
     // Get 1st arg
     TheChild = getChild(TheChild, 1, 4);
@@ -45,22 +60,29 @@ const DeclRefExpr *MemberRefCollector::unwrapCallbackArg(const Expr *CallbackArg
                     "Reference operator arg is not a decl ref"))
         return nullptr;
 
-    return static_cast<const DeclRefExpr *>(TheChild);
+    const Stmt *CallbackDef = this->getCallbackDefinition(static_cast<const DeclRefExpr *>(TheChild));
+    return static_cast<const Expr *>(CallbackDef);
 }
 
-void MemberRefCollector::getCallbackDefinition(const DeclRefExpr *CallbackSignature) {
+const Stmt *MemberRefCollector::getCallbackDefinition(const DeclRefExpr *CallbackSignature) {
     if (!checkClass(CallbackSignature, Stmt::StmtClass::DeclRefExprClass,
                     "Callback signature is null"))
-        return;
+        return nullptr;
 
     const ValueDecl *CallbackDecl = CallbackSignature->getDecl();
-    if (CallbackDecl == nullptr || CallbackDecl->getKind() != Decl::CXXMethod) return;
+    if (CallbackDecl == nullptr || CallbackDecl->getKind() != Decl::CXXMethod) return nullptr;
     const CXXMethodDecl *CXXDecl = dynamic_cast<const CXXMethodDecl *>(CallbackDecl->getAsFunction());
-    CXXDecl->getBody()->dumpColor();
+    return CXXDecl->getBody();
 }
 
-void MemberRefCollector::getCallbackMemberRefs(const Expr *CallbackBody) {
+void MemberRefCollector::getCallbackMemberRefs(Expr *Expression, std::vector<MemberExpr *> &Accumulator) {
+    if (Expression->getStmtClass() == Stmt::StmtClass::MemberExprClass) {
+        Accumulator.push_back(static_cast<MemberExpr *>(Expression));
+        return;
+    }
 
+    for (Stmt *Child: Expression->children())
+        this->getCallbackMemberRefs(static_cast<Expr *>(Child), Accumulator);
 }
 
 const Expr *getChild(const Expr *Parent, int ChildIndex, int ExpectedNChildren) {
--- MemberRefCollector.hh
+++ MemberRefCollector.hh
@@ -12,19 +12,20 @@
 using namespace clang;
 using namespace clang::ast_matchers;
 
-const Expr * getChild(const Expr* Parent, int ChildIndex, int ExpectedNChildren);
-bool checkClass(const Expr* Expression, const Stmt::StmtClass &ExpectedClass, const std::string &NotMatchingMessage);
+const Expr *getChild(const Expr *Parent, int ChildIndex, int ExpectedNChildren);
+
+bool checkClass(const Expr *Expression, const Stmt::StmtClass &ExpectedClass, const std::string &NotMatchingMessage);
 
 class MemberRefCollector : public MatchFinder::MatchCallback {
 public :
-  virtual void run(const MatchFinder::MatchResult &Result);
+    virtual void run(const MatchFinder::MatchResult &Result);
 
 private:
-  const DeclRefExpr * unwrapCallbackArg(const Expr* callbackArg);
-  void getCallbackDefinition(const Expr* callbackSignature);
-  void getCallbackMemberRefs(const Expr* callbackBody);
+    const Expr * unwrapCallbackArg(const Expr *CallbackArg);
 
-    void getCallbackDefinition(const DeclRefExpr *CallbackSignature);
+    const Stmt * getCallbackDefinition(const DeclRefExpr *CallbackSignature);
+
+    void getCallbackMemberRefs(Expr *Expression, std::vector<MemberExpr*> &Accumulator);
 };
 
 #endif // LLVM_MEMBER_REF_COLLECTOR_HH
