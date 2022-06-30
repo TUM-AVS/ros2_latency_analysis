7f45427ebed94862aacede3ad0aaba4faaed383e
Maximilian Schmeller <m.schmeller@tum.de>
2022-06-30 18:39:26 +0200

    Fixed yaml compliance (forgot ':' after list entries)

--- DataWriter.cpp
+++ DataWriter.cpp
@@ -5,7 +5,7 @@
 #include "DataWriter.h"
 
 void schmeller::ROS2DepCheck::DataWriter::writeFunction(const FunctionDecl *Decl, const MatchFinder::MatchResult &Result) {
-    OutStream << lvl(0) << "- " << Decl->getID() << '\n';
+    OutStream << lvl(0) << "- " << Decl->getID() << ":\n";
     OutStream << lvl(2) << "type: function\n";
     OutStream << lvl(2) << "qualified_name: " << Decl->getQualifiedNameAsString() << '\n';
     OutStream << lvl(2) << "source_range: " << Decl->getSourceRange().printToString(*Result.SourceManager) << '\n';
@@ -14,7 +14,7 @@ void schmeller::ROS2DepCheck::DataWriter::writeFunction(const FunctionDecl *Decl
     std::vector<MemberExpr *> CallbackMemberRefs;
     getCallbackMemberRefs((Expr *) getCallbackBody(Decl), CallbackMemberRefs);
     for (MemberExpr *MemberRef: CallbackMemberRefs) {
-        OutStream << lvl(3) << "- " << MemberRef->getMemberDecl()->getID() << '\n';
+        OutStream << lvl(3) << "- " << MemberRef->getMemberDecl()->getID() << ":\n";
         OutStream << lvl(5) << "source_range:" << MemberRef->getSourceRange().printToString(*Result.SourceManager) << '\n';
         OutStream << lvl(5) << "is_lvalue: " << MemberRef->isLValue() << '\n';
         OutStream << lvl(5) << "is_glvalue: " << MemberRef->isGLValue() << '\n';
@@ -32,21 +32,21 @@ void schmeller::ROS2DepCheck::DataWriter::writeLambda(const LambdaExpr *Decl, co
 }
 
 void schmeller::ROS2DepCheck::DataWriter::writeNode(const CXXRecordDecl *Decl, const MatchFinder::MatchResult &Result) {
-    OutStream << lvl(0) << "- " << Decl->getID() << '\n';
+    OutStream << lvl(0) << "- " << Decl->getID() << ":\n";
     OutStream << lvl(2) << "type: node\n";
     OutStream << lvl(2) << "qualified_name: " << Decl->getQualifiedNameAsString() << '\n';
     OutStream << lvl(2) << "source_range: " << Decl->getSourceRange().printToString(*Result.SourceManager) << '\n';
 
     OutStream << lvl(2) << "fields:" << '\n';
     for (const auto* Field : Decl->fields()) {
-        OutStream << lvl(3) << "- " << Field->getID() << '\n';
+        OutStream << lvl(3) << "- " << Field->getID() << ":\n";
         OutStream << lvl(5) << "qualified_name: " << Field->getQualifiedNameAsString() << '\n';
         OutStream << lvl(5) << "source_range: " << Field->getSourceRange().printToString(*Result.SourceManager) << '\n';
     }
 
     OutStream << lvl(2) << "methods:" << '\n';
     for (const auto* Method : Decl->methods()) {
-        OutStream << lvl(3) << "- " << Method->getID() << '\n';
+        OutStream << lvl(3) << "- " << Method->getID() << ":\n";
         OutStream << lvl(5) << "qualified_name: " << Method->getQualifiedNameAsString() << '\n';
         OutStream << lvl(5) << "source_range: " << Method->getSourceRange().printToString(*Result.SourceManager) << '\n';
     }
