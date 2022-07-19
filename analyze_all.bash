3f9d67ace720f22f1af3a1178348c1f19765d890
Maximilian Schmeller <m.schmeller@tum.de>
2022-07-19 13:05:14 +0200

    Better analyze_all output

--- analyze_all.bash
+++ analyze_all.bash
@@ -10,37 +10,42 @@ out_dir="$cur_dir/output"
 mkdir -p $out_dir
 
 node_tus=$(find $source_dir -name *node.cpp)
+total_tus=$(find $source_dir -name *node.cpp | wc -l)
 
-worker_task(){
-  source_dir=$1
-  build_dir=$2
-  out_dir=$3
-  node_tu=$4
+worker_task() {
+  my_j=$1
+  source_dir=$2
+  build_dir=$3
+  out_dir=$4
+  node_tu=$5
 
   out_name=${node_tu//\//-}
-  json_name=$(realpath --relative-to $source_dir $node_tu)
-  json_name=${json_name//\//-}
+  rel_name=$(realpath --relative-to $source_dir $node_tu)
+  json_name=${rel_name//\//-}
   json_name=${json_name//'.cpp'/'.json'}
 
-  i=$(($i + 1))
-  echo "$i $out_name"
+  echo "$my_j/$total_tus $rel_name"
 
   tu_in_db=$(cat $build_dir/compile_commands.json | grep "$node_tu")
-  if [ -z "$tu_in_db" ]
-  then
-    echo "File not found in compile DB: $(realpath --relative-to $source_dir $node_tu)"
-    echo "null" > "$out_dir/$json_name"
+  if [ -z "$tu_in_db" ]; then
+    echo -e "  \e[31mFile not found in compile DB: $(realpath --relative-to $source_dir $node_tu)\e[0m"
+    echo "null" >"$out_dir/$json_name"
   else
-    ./dep-check --extra-arg=-w -p $build_dir $node_tu > /dev/null
+    ./dep-check --extra-arg=-w -p $build_dir $node_tu >/dev/null
     mv "./$out_name" "$out_dir/$json_name"
   fi
 }
 
 N=8
 i=0
+j=0
 (
-for node_tu in $node_tus ; do
-   ((i=i%N)); ((i++==0)) && wait
-   worker_task $source_dir $build_dir $out_dir $node_tu &
-done
+  for node_tu in $node_tus; do
+    ((j++))
+    ((i = i % N))
+    ((i++ == 0)) && wait
+    worker_task $j $source_dir $build_dir $out_dir $node_tu &
+  done
 )
+
+echo "Done."
