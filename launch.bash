6c44d8fa05d7e956b222ee6fe5e808c4c59d2295
Maximilian Schmeller <m.schmeller@tum.de>
2022-12-13 21:22:25 +0900

    Destroy eventually remaining tracing sessions

--- launch.bash
+++ launch.bash
@@ -61,6 +61,7 @@ echo "[LAUNCHER] Launched sim worker on ${sim_username}@${sim_hostname}"
     ssh -i "$ssh_id" "${aw_username}"@"${aw_hostname}" "rm -f ${aw_rootdir}/scenario_runner/worker.log" &&
     ssh -tt -i "$ssh_id" "${aw_username}"@"${aw_hostname}" "screen -L -Logfile ${aw_rootdir}/scenario_runner/worker.log -S aw_orchestrator timeout -k20 260 ${aw_rootdir}/scenario_runner/worker.bash aw $cfg_path" > /dev/null &&
     ssh -i "$ssh_id" "${aw_username}"@"${aw_hostname}" "pkill --signal SIGKILL -f 'ros|http.server|aw_orchestrator'"
+    ssh -i "$ssh_id" "${aw_username}"@"${aw_hostname}" "lttng destroy max-ma-trace || true"
 ) &
 pids+=($!)
 echo "[LAUNCHER] Launched aw worker on ${aw_username}@${aw_hostname}"
