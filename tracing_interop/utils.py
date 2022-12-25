0662386732946218530d054cec5d3250cafada09
Maximilian Schmeller <m.schmeller@tum.de>
2022-12-25 20:39:44 +0900

    Support colum renaming and make argument names clearer in df_to_type_list

--- tracing_interop/utils.py
+++ tracing_interop/utils.py
@@ -6,10 +6,15 @@ def row_to_type(row, type, **type_kwargs):
     return type(**row, **type_kwargs)
 
 
-def df_to_type_list(df, type, mappers=None, **type_kwargs):
-    if mappers is not None:
-        for col, mapper in mappers.items():
+def df_to_type_list(df, type, column_value_mappers=None, column_to_field_mappings=None, **type_kwargs):
+    if column_value_mappers is not None:
+        for col, mapper in column_value_mappers.items():
             df[col] = df[col].map(mapper)
+
+    if column_to_field_mappings is not None:
+        for col, field in column_to_field_mappings.items():
+            df[field] = df[col]
+            del df[col]
 
     has_idx = not isinstance(df.index, pd.RangeIndex)
     ret_list = []
